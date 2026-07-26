4982535dadfe1e1b85b7fbfbfe4314aca0db90e3
shpark <shpark@gmail.com>
2026-07-26 17:05:46 +0900

    first commit

---
+++ src/test/java/com/edu/eduplatform/EduplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.eduplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduplatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
